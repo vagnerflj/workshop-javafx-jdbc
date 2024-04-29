d5995a51c78ae24a3100233d3608512dc2d9f450
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-29 09:58:45 -0300

    Department Service

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,8 @@ module workshopjavafxjbdc {
     requires javafx.controls;
     requires java.sql;
 	requires javafx.fxml;
-	requires javafx.graphics; // Add this line for JDBC
+	requires javafx.graphics;
+	requires javafx.base; // Add this line for JDBC
     
     opens application to javafx.graphics, javafx.fxml;
 }
